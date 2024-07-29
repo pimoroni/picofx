c1f1dc5776fda7933ab9988889ed53d220d0ec29
Phil Howard <phil@gadgetoid.com>
2024-07-29 14:59:34 +0100

    TinyFX: Make 4MB the default and drop 2MB.

--- boards/PIMORONI_TINYFX/mpconfigboard.h
+++ boards/PIMORONI_TINYFX/mpconfigboard.h
@@ -1,3 +1,3 @@
 // Board and hardware specific configuration
-#define MICROPY_HW_BOARD_NAME                   "Raspberry Pi Pico"
+#define MICROPY_HW_BOARD_NAME                   "Pimoroni TinyFX 4MB"
 #define MICROPY_HW_FLASH_STORAGE_BYTES          (1408 * 1024)
--- boards/PIMORONI_TINYFX/mpconfigvariant_FLASH_4M.cmake
+++
@@ -1,6 +0,0 @@
-set(PICO_BOARD "pimoroni_tinyfx_4mb")
-
-# Override the MicroPython board name
-list(APPEND MICROPY_DEF_BOARD
-    MICROPY_HW_BOARD_NAME="Pimoroni TinyFX 4MB"
-)
