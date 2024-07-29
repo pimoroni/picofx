7d7dab7a8c2bd3f1bfb61d51c81d4b1dd620c11f
Phil Howard <phil@gadgetoid.com>
2024-07-29 17:20:16 +0100

    Don't freeze audio or tiny_fx.

--- boards/PIMORONI_TINYFX/manifest.py
+++ boards/PIMORONI_TINYFX/manifest.py
@@ -1,6 +1,6 @@
 include("$(PORT_DIR)/boards/manifest.py")
 
-freeze("lib/")
+freeze("frozen_libs/")
 
 # TODO: Convince Chris this is the one true and holy path
 # package("picofx", base_path="../../")
