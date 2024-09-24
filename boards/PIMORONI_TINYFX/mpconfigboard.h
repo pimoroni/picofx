ec13ed4e869460fce0fa677c1cf3b49b498d626e
Phil Howard <github@gadgetoid.com>
2024-09-24 17:37:25 +0100

    Both: Rework firmware/filesystem sizes.
    The W build needs additional space for the wireless firmware.
    
    This is painful since it makes these builds non-interchangable
    as the filesystem will move/be overwritten.
    
    As such, change *only* the W build and avoid changing the existing
    build so updates don't delete user files.
    
    Additionally rework the sizes so that it's clearer we're
    reserving space for the firmware and not just making up numbers.

--- boards/PIMORONI_TINYFX/mpconfigboard.h
+++ boards/PIMORONI_TINYFX/mpconfigboard.h
@@ -3,9 +3,12 @@
 // Might be defined by mpconfigvariant.cmake
 #define MICROPY_HW_BOARD_NAME                   "Pimoroni TinyFX"
 #endif
-#define MICROPY_HW_FLASH_STORAGE_BYTES          (3 * 1024 * 1024)
 
 #if defined(MICROPY_PY_NETWORK_CYW43)
+
+// We need space for networking firmware on network builds
+// 1536 * 1024 = 1.5MB
+#define FIRMWARE_SIZE_BYTES                     (1536 * 1024)
 
 // CYW43 driver configuration.
 #define CYW43_USE_SPI                           (1)
@@ -13,4 +16,11 @@
 #define CYW43_GPIO                              (0)
 #define CYW43_SPI_PIO                           (1)
 
+#else
+
+// 1MB for the firmware
+#define FIRMWARE_SIZE_BYTES                     (1 * 1024 * 1024)
+
 #endif
+
+#define MICROPY_HW_FLASH_STORAGE_BYTES          (PICO_FLASH_SIZE_BYTES - FIRMWARE_SIZE_BYTES)
