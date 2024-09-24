9671a4851c5cdb111ed2975ca47750c477c8775f
ZodiusInfuser <christopher.parrott2@gmail.com>
2024-09-24 15:52:28 +0100

    Initial test

--- boards/PIMORONI_TINYFX/mpconfigboard.h
+++ boards/PIMORONI_TINYFX/mpconfigboard.h
@@ -1,3 +1,16 @@
 // Board and hardware specific configuration
+#ifndef MICROPY_HW_BOARD_NAME
+// Might be defined by mpconfigvariant.cmake
 #define MICROPY_HW_BOARD_NAME                   "Pimoroni TinyFX"
+#endif
 #define MICROPY_HW_FLASH_STORAGE_BYTES          (3 * 1024 * 1024)
+
+#if defined(MICROPY_PY_NETWORK_CYW43)
+
+// CYW43 driver configuration.
+#define CYW43_USE_SPI                           (1)
+#define CYW43_LWIP                              (1)
+#define CYW43_GPIO                              (0)
+#define CYW43_SPI_PIO                           (1)
+
+#endif
