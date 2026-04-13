2f1b70c9bd0bb5d3ba46e01ca507425e66fb5631
ZodiusInfuser <christopher.parrott2@gmail.com>
2026-04-13 15:00:10 +0100

    Add networking libraries

--- boards/PIMORONI_MIGHTYFX/manifest.py
+++ boards/PIMORONI_MIGHTYFX/manifest.py
@@ -1,5 +1,10 @@
 include("$(PORT_DIR)/boards/manifest.py")
 
+require("bundle-networking")
+
+# Bluetooth
+require("aioble")
+
 freeze("frozen_libs/")
 
 # TODO: Convince Chris this is the one true and holy path
