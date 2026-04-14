1f740875073a341d7063df80a68cd48f36e99f7a
Phil Howard <github@gadgetoid.com>
2026-04-14 18:47:24 +0100

    CI: Move filesystem staging to CMake.
    Use CMake to prepare the UF2 staging filesystem.

--- boards/PIMORONI_MIGHTYFX/mpconfigboard.cmake
+++ boards/PIMORONI_MIGHTYFX/mpconfigboard.cmake
@@ -11,6 +11,13 @@ set(MICROPY_C_HEAP_SIZE 4096)
 
 set(PICO_NUM_GPIOS 48)
 
+# List of directories to copy into the UF2 staging dir
+# The contents of these will be available for packing into the filesystem build
+list(APPEND UF2_COPY_DIRS
+    "${CMAKE_CURRENT_LIST_DIR}/../../examples/mighty_fx"
+    "${CMAKE_CURRENT_LIST_DIR}/../../picofx"
+    "${CMAKE_CURRENT_LIST_DIR}/visible_libs"
+)
+
 set(PIMORONI_UF2_MANIFEST ${CMAKE_CURRENT_LIST_DIR}/uf2-manifest.txt)
-set(PIMORONI_UF2_DIR ${CMAKE_CURRENT_BINARY_DIR}/filesystem)
 include(${CMAKE_CURRENT_LIST_DIR}/../common.cmake)
--- boards/common.cmake
+++ boards/common.cmake
@@ -6,9 +6,38 @@ find_package (Python COMPONENTS Interpreter Development)
 
 message("dir2uf2/py_decl: Using Python ${Python_EXECUTABLE}")
 
+set(UF2_STAGING_DIR "${CMAKE_CURRENT_BINARY_DIR}/filesystem")
+
 # Convert supplies paths to absolute, for a quieter life
 get_filename_component(PIMORONI_UF2_MANIFEST ${PIMORONI_UF2_MANIFEST} REALPATH)
-get_filename_component(PIMORONI_UF2_DIR ${PIMORONI_UF2_DIR} REALPATH)
+
+# Create filesystem directory
+file(MAKE_DIRECTORY ${UF2_STAGING_DIR})
+
+function(copy_recursive SOURCE_PATH DESTINATION_PATH REGEX)
+
+    file(GLOB_RECURSE
+        FILES
+        ${SOURCE_PATH}
+        "${SOURCE_PATH}/${REGEX}")
+
+    foreach(file ${FILES})
+        file(RELATIVE_PATH RELATIVE_FILE_PATH ${SOURCE_PATH} ${file})
+        get_filename_component(FOLDER ${RELATIVE_FILE_PATH} DIRECTORY ${SOURCE_PATH})
+        file(MAKE_DIRECTORY ${DESTINATION_PATH}/${FOLDER} )
+        message(STATUS "Copy file ${file} -> ${DESTINATION_PATH}/${RELATIVE_FILE_PATH}")
+        configure_file(${file} "${DESTINATION_PATH}/${RELATIVE_FILE_PATH}" COPYONLY)
+    endforeach()
+
+endfunction()
+
+
+# Prepare LittleFS filesystem
+foreach(dir in ${UF2_COPY_DIRS})
+    get_filename_component(dir "${dir}" REALPATH)
+    message(STATUS "Copy directory ${dir} -> ${UF2_STAGING_DIR}")
+    copy_recursive("${dir}" "${UF2_STAGING_DIR}" "*")
+endforeach()
 
 if (EXISTS "${PIMORONI_TOOLS_DIR}/py_decl/py_decl.py")
     add_custom_target("${MICROPY_TARGET}-verify" ALL
@@ -19,11 +48,11 @@ if (EXISTS "${PIMORONI_TOOLS_DIR}/py_decl/py_decl.py")
     )
 endif()
 
-if (EXISTS "${PIMORONI_TOOLS_DIR}/dir2uf2/dir2uf2" AND EXISTS "${PIMORONI_UF2_MANIFEST}" AND EXISTS "${PIMORONI_UF2_DIR}")
+if (EXISTS "${PIMORONI_TOOLS_DIR}/dir2uf2/dir2uf2" AND EXISTS "${PIMORONI_UF2_MANIFEST}" AND EXISTS "${UF2_STAGING_DIR}")
     MESSAGE("dir2uf2: Using manifest ${PIMORONI_UF2_MANIFEST}.")
     MESSAGE("dir2uf2: Using root ${PIMORONI_UF2_DIR}.")
     add_custom_target("${MICROPY_TARGET}-with-filesystem.uf2" ALL
-        COMMAND ${Python_EXECUTABLE} "${PIMORONI_TOOLS_DIR}/dir2uf2/dir2uf2" --fs-compact --sparse --append-to "${MICROPY_TARGET}.uf2" --manifest "${PIMORONI_UF2_MANIFEST}" --filename with-libs-and-examples.uf2 "${PIMORONI_UF2_DIR}"
+        COMMAND ${Python_EXECUTABLE} "${PIMORONI_TOOLS_DIR}/dir2uf2/dir2uf2" --fs-compact --sparse --append-to "${MICROPY_TARGET}.uf2" --manifest "${PIMORONI_UF2_MANIFEST}" --filename with-libs-and-examples.uf2 "${UF2_STAGING_DIR}"
         WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
         COMMENT "dir2uf2: Appending filesystem to ${MICROPY_TARGET}.uf2."
         DEPENDS ${MICROPY_TARGET}
