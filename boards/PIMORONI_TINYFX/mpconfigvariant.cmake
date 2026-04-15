2d87b7643de44a3a64338670481d91068757cc92
Phil Howard <github@gadgetoid.com>
2026-04-15 14:17:01 +0100

    CI: Move filesystem staging to a shell script.
    It was complicated and ungainly to replicate this in CMake. Use a custom command instead.

--- boards/PIMORONI_TINYFX/mpconfigvariant.cmake
+++ boards/PIMORONI_TINYFX/mpconfigvariant.cmake
@@ -1,10 +1,3 @@
-# List of directories to copy into the UF2 staging dir
-# The contents of these will be available for packing into the filesystem build
-list(APPEND UF2_COPY_DIRS
-    "${CMAKE_CURRENT_LIST_DIR}/../../examples/tiny_fx"
-    "${CMAKE_CURRENT_LIST_DIR}/../../picofx"
-    "${CMAKE_CURRENT_LIST_DIR}/visible_libs"
-)
-
+set(UF2_STAGING_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/uf2-copyfiles.sh)
 set(PIMORONI_UF2_MANIFEST ${CMAKE_CURRENT_LIST_DIR}/uf2-manifest.txt)
 include(${CMAKE_CURRENT_LIST_DIR}/../common.cmake)
--- boards/common.cmake
+++ boards/common.cmake
@@ -11,34 +11,6 @@ set(UF2_STAGING_DIR "${CMAKE_CURRENT_BINARY_DIR}/filesystem")
 # Convert supplies paths to absolute, for a quieter life
 get_filename_component(PIMORONI_UF2_MANIFEST ${PIMORONI_UF2_MANIFEST} REALPATH)
 
-# Create filesystem directory
-file(MAKE_DIRECTORY ${UF2_STAGING_DIR})
-
-function(copy_recursive SOURCE_PATH DESTINATION_PATH REGEX)
-
-    file(GLOB_RECURSE
-        FILES
-        ${SOURCE_PATH}
-        "${SOURCE_PATH}/${REGEX}")
-
-    foreach(file ${FILES})
-        file(RELATIVE_PATH RELATIVE_FILE_PATH ${SOURCE_PATH} ${file})
-        get_filename_component(FOLDER ${RELATIVE_FILE_PATH} DIRECTORY ${SOURCE_PATH})
-        file(MAKE_DIRECTORY ${DESTINATION_PATH}/${FOLDER} )
-        message(STATUS "Copy file ${file} -> ${DESTINATION_PATH}/${RELATIVE_FILE_PATH}")
-        configure_file(${file} "${DESTINATION_PATH}/${RELATIVE_FILE_PATH}" COPYONLY)
-    endforeach()
-
-endfunction()
-
-
-# Prepare LittleFS filesystem
-foreach(dir in ${UF2_COPY_DIRS})
-    get_filename_component(dir "${dir}" REALPATH)
-    message(STATUS "Copy directory ${dir} -> ${UF2_STAGING_DIR}")
-    copy_recursive("${dir}" "${UF2_STAGING_DIR}" "*")
-endforeach()
-
 if (EXISTS "${PIMORONI_TOOLS_DIR}/py_decl/py_decl.py")
     add_custom_target("${MICROPY_TARGET}-verify" ALL
         COMMAND ${Python_EXECUTABLE} "${PIMORONI_TOOLS_DIR}/py_decl/py_decl.py" --to-json --verify "${CMAKE_CURRENT_BINARY_DIR}/${MICROPY_TARGET}.uf2"
@@ -48,20 +20,35 @@ if (EXISTS "${PIMORONI_TOOLS_DIR}/py_decl/py_decl.py")
     )
 endif()
 
-if (EXISTS "${PIMORONI_TOOLS_DIR}/dir2uf2/dir2uf2" AND EXISTS "${PIMORONI_UF2_MANIFEST}" AND EXISTS "${UF2_STAGING_DIR}")
+if (EXISTS "${PIMORONI_TOOLS_DIR}/dir2uf2/dir2uf2" AND EXISTS "${PIMORONI_UF2_MANIFEST}" AND EXISTS "${UF2_STAGING_SCRIPT}")
     MESSAGE(STATUS "dir2uf2: Using manifest ${PIMORONI_UF2_MANIFEST}.")
     MESSAGE(STATUS "dir2uf2: Using root ${UF2_STAGING_DIR}.")
+
+    # Create filesystem directory
+    file(MAKE_DIRECTORY ${UF2_STAGING_DIR})
+
+    # Set --sparse for RP2350 builds
     if(PICO_PLATFORM STREQUAL "rp2350")
         message(STATUS "dir2uf2: Building sparse UF2 (rp2350 only)")
         set(UF2_SPARSE "--sparse")
     else()
         set(UF2_SPARSE "")
     endif()
-    add_custom_target("${MICROPY_TARGET}-with-filesystem.uf2" ALL
+
+    # Add a target to prep the staging filesystem
+    add_custom_target("${MICROPY_TARGET}-staging" ALL
+        COMMAND bash "${UF2_STAGING_SCRIPT}" "${UF2_STAGING_DIR}"
+        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+        COMMENT "dir2uf2: Preparing staging filesystem."
+        DEPENDS ${MICROPY_TARGET}
+        DEPENDS "${MICROPY_TARGET}-verify")
+
+    # Add a target to prep the build
+    add_custom_target("${MICROPY_TARGET}-with-libs-and-examples.uf2" ALL
         COMMAND ${Python_EXECUTABLE} "${PIMORONI_TOOLS_DIR}/dir2uf2/dir2uf2" --fs-compact ${UF2_SPARSE} --append-to "${MICROPY_TARGET}.uf2" --manifest "${PIMORONI_UF2_MANIFEST}" --filename with-libs-and-examples.uf2 "${UF2_STAGING_DIR}"
         WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
         COMMENT "dir2uf2: Appending filesystem to ${MICROPY_TARGET}.uf2."
         DEPENDS ${MICROPY_TARGET}
-        DEPENDS "${MICROPY_TARGET}-verify"
+        DEPENDS "${MICROPY_TARGET}-staging"
     )
 endif()
