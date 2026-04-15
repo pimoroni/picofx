b83066945d663e159e77c410a7dcfb4ea6e3a622
Phil Howard <github@gadgetoid.com>
2026-04-15 14:17:01 +0100

    CI: Move filesystem staging to a shell script.
    It was complicated and ungainly to replicate this in CMake. Use a custom command instead.

--- boards/PIMORONI_TINYFX/mpconfigvariant_w.cmake
+++ boards/PIMORONI_TINYFX/mpconfigvariant_w.cmake
@@ -37,14 +37,6 @@ set(MICROPY_FROZEN_MANIFEST ${MICROPY_BOARD_DIR}/manifest_w.py)
 set(PICO_BOARD "pimoroni_tinyfx")
 set(PICO_BOARD_HEADER_DIRS ${CMAKE_CURRENT_LIST_DIR})
 
-# List of directories to copy into the UF2 staging dir
-# The contents of these will be available for packing into the filesystem build
-list(APPEND UF2_COPY_DIRS
-    "${CMAKE_CURRENT_LIST_DIR}/../../examples/tiny_fx"
-    "${CMAKE_CURRENT_LIST_DIR}/../../examples/tiny_fx_w"
-    "${CMAKE_CURRENT_LIST_DIR}/../../picofx"
-    "${CMAKE_CURRENT_LIST_DIR}/visible_libs"
-)
-
+set(UF2_STAGING_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/uf2-copyfiles-w.sh)
 set(PIMORONI_UF2_MANIFEST ${CMAKE_CURRENT_LIST_DIR}/uf2-manifest-w.txt)
 include(${CMAKE_CURRENT_LIST_DIR}/../common.cmake)
---
+++ boards/PIMORONI_TINYFX/uf2-copyfiles-w.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+TARGET=$1
+
+SCRIPT_PATH=${BASH_SOURCE-$0}
+SCRIPT_PATH=$(dirname "$SCRIPT_PATH")
+
+cp -r -v "$SCRIPT_PATH/../../examples/tiny_fx/" "$TARGET/"
+cp -r -v "$SCRIPT_PATH/../../examples/tiny_fx_w/" "$TARGET/"
+
+mkdir -p "$TARGET/lib"
+cp -r -v "$SCRIPT_PATH/../../picofx" "$TARGET/lib"
+cp -r -v "$SCRIPT_PATH/visible_libs/" "$TARGET/lib"
+
+rm -f "$TARGET/lib/picofx/README.md"
+rm -f "$TARGET/README.md"
