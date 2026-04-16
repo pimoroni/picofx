5692b993566285ee2598d6513b7e042530fad0bf
ZodiusInfuser <christopher.parrott2@gmail.com>
2026-04-16 14:44:02 +0100

    Improve removal messaging

--- boards/PIMORONI_TINYFX/uf2-copyfiles.sh
+++ boards/PIMORONI_TINYFX/uf2-copyfiles.sh
@@ -13,4 +13,10 @@ cp -r -v "$SCRIPT_PATH/../../picofx" "$TARGET/lib"
 cp -r -v "$SCRIPT_PATH/../../../aye_arr/aye_arr" "$TARGET/lib"
 
 # Remove any markdown files
-find "$TARGET" -type f -name '*.md' -print -delete
+find "$TARGET" -type f -name '*.md' -exec sh -c '
+  if rm -- "$1"; then
+    printf "%s deleted\n" "$1"
+  else
+    printf "%s failed to delete\n" "$1" >&2
+  fi
+' sh {} \;
