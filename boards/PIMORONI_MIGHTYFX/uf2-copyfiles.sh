070127f8e2f9d9f0ee73c11bef2512da8e83cc90
ZodiusInfuser <christopher.parrott2@gmail.com>
2026-04-17 11:38:12 +0100

    CI: Move common frozen & visible libs to shared folder

--- boards/PIMORONI_MIGHTYFX/uf2-copyfiles.sh
+++ boards/PIMORONI_MIGHTYFX/uf2-copyfiles.sh
@@ -5,11 +5,12 @@ TARGET=$1
 SCRIPT_PATH=${BASH_SOURCE-$0}
 SCRIPT_PATH=$(dirname "$SCRIPT_PATH")
 
-cp -r -v "$SCRIPT_PATH/../../examples/mighty_fx/" "$TARGET/"
+cp -r -v "$SCRIPT_PATH/../../examples/mighty_fx/." "$TARGET/"
 
 mkdir -p "$TARGET/lib"
 cp -r -v "$SCRIPT_PATH/../../picofx" "$TARGET/lib"
 cp -r -v "$SCRIPT_PATH/visible_libs/." "$TARGET/lib"
+cp -r -v "$SCRIPT_PATH/../visible_libs/." "$TARGET/lib"
 cp -r -v "$SCRIPT_PATH/../../../aye_arr/aye_arr" "$TARGET/lib"
 
 # Remove any markdown files
