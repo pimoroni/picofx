c8c7caf1fb7812c46bc4c167dc1e7847b68e1818
ZodiusInfuser <christopher.parrott2@gmail.com>
2026-05-01 13:22:18 +0100

    Create a PseudoLED to allow for I2C comms to interact with effects sytem

--- picofx/__init__.py
+++ picofx/__init__.py
@@ -31,16 +31,34 @@ def rgb_from_hsv(h, s, v):
             return v, p, q
 
 
-# A basic wrapper for PWM with regular on/off and toggle functions from Pin
-# Intended to be used for driving LEDs with brightness control & compatibility with Pin
-class PWMLED:
-    def __init__(self, pin, invert=False, gamma=1):
-        self.__gamma = gamma
-        self.__led = PWM(Pin(pin), freq=1000, duty_u16=0, invert=invert)
+# A pseudo LED class for storing brightness. For use in comms
+class PseudoLED:
+    def __init__(self):
         self.__brightness = 0
 
     def brightness(self, brightness):
         self.__brightness = min(1.0, max(0.0, brightness))
+
+    def on(self):
+        self.brightness(1)
+
+    def off(self):
+        self.brightness(0)
+
+    def toggle(self):
+        self.brightness(1 - self.__brightness)
+
+
+# A basic wrapper for PWM with regular on/off and toggle functions from Pin
+# Intended to be used for driving LEDs with brightness control & compatibility with Pin
+class PWMLED(PseudoLED):
+    def __init__(self, pin, invert=False, gamma=1):
+        super().__init__()
+        self.__gamma = gamma
+        self.__led = PWM(Pin(pin), freq=1000, duty_u16=0, invert=invert)
+
+    def brightness(self, brightness):
+        super().brightness(brightness)
         self.__led.duty_u16(int(pow(self.__brightness, self.__gamma) * 65535 + 0.5))
 
     def on(self):
@@ -55,9 +73,9 @@ class PWMLED:
 
 class RGBLED:
     def __init__(self, r, g, b, invert=True, gamma=1):
-        self.led_r = r if isinstance(r, PWMLED) else PWMLED(r, invert=invert, gamma=gamma)
-        self.led_g = g if isinstance(g, PWMLED) else PWMLED(g, invert=invert, gamma=gamma)
-        self.led_b = b if isinstance(b, PWMLED) else PWMLED(b, invert=invert, gamma=gamma)
+        self.led_r = r if isinstance(r, PseudoLED) else PWMLED(r, invert=invert, gamma=gamma)
+        self.led_g = g if isinstance(g, PseudoLED) else PWMLED(g, invert=invert, gamma=gamma)
+        self.led_b = b if isinstance(b, PseudoLED) else PWMLED(b, invert=invert, gamma=gamma)
 
     def __rgb(self, r, g, b):
         self.led_r.brightness(r)
