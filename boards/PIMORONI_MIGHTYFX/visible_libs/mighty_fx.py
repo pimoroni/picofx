1aabf5b1f17f1e9065a1d8351fe6623bccf44013
ZodiusInfuser <christopher.parrott2@gmail.com>
2026-07-28 17:59:51 +0100

    spidisplay: modify MightyFX to make screen experiments easier

--- boards/PIMORONI_MIGHTYFX/visible_libs/mighty_fx.py
+++ boards/PIMORONI_MIGHTYFX/visible_libs/mighty_fx.py
@@ -80,22 +80,22 @@ class MightyFX:
 
     RGB_GAMMA = 2.2
 
-    def __init__(self, spce_a=None, spce_b=None, init_i2c=True, init_wav=True, wav_root="/", native_display=False, bands=None):
+    def __init__(self, spce_a=None, spce_b=None, init_i2c=True, init_wav=True, wav_root="/", native_display=False, sdef_a=None, sdef_b=None):
         # Set up the mono and RGB LED outputs
         self.outputs = [RGBLED(*out, invert=False, gamma=self.RGB_GAMMA) for out in self.OUT_PINS]
 
         self.screen_a = None
         self.motors_a = None
         if spce_a in [SPCE.SCREEN_154, SPCE.SCREEN_280]:
-            sdef = screen_defs[spce_a]
+            sdef = screen_defs[spce_a] if sdef_a is None else sdef_a
             self.bl_a = Pin.board.SPCE_A_BL
 
             if native_display:
                 from spidisplay import SPIDisplay
                 disp_a = SPIDisplay(spi=0, sck=self.SPCE_A_SCK_PIN, mosi=self.SPCE_A_MOSI_PIN,
                                     cs=self.SPCE_A_CS_PIN, dc=self.SPCE_A_DC_PIN, baudrate=sdef.baud,
-                                    bitdepth=sdef.bits, band_lines=sdef.bands if bands is None else bands,
-                                    cache_columns=sdef.columns, spi_frame_bits=sdef.spi_bits)
+                                    bitdepth=sdef.bits, band_lines=sdef.bands, cache_columns=sdef.columns,
+                                    spi_frame_bits=sdef.spi_bits)
                 self.screen_a = ST7789(None, None, None, self.bl_a, sdef.width, sdef.height, sdef.bits, sdef.fps, display=disp_a)
             else:
                 spi_a = SPI(id=0, baudrate=sdef.baud, sck=Pin.board.SPCE_A_SCK, mosi=Pin.board.SPCE_A_MOSI)
@@ -115,18 +115,18 @@ class MightyFX:
         self.screen_b = None
         self.motors_b = None
         if spce_b in [SPCE.SCREEN_154, SPCE.SCREEN_280]:
-            sdef = screen_defs[spce_b]
+            sdef = screen_defs[spce_b] if sdef_b is None else sdef_b
             self.bl_b = Pin.board.SPCE_B_BL
 
             if native_display:
                 from spidisplay import SPIDisplay
                 disp_b = SPIDisplay(spi=1, sck=self.SPCE_B_SCK_PIN, mosi=self.SPCE_B_MOSI_PIN,
                                     cs=self.SPCE_B_CS_PIN, dc=self.SPCE_B_DC_PIN, baudrate=sdef.baud,
-                                    bitdepth=sdef.bits, band_lines=sdef.bands if bands is None else bands,
-                                    cache_columns=sdef.columns, spi_frame_bits=sdef.spi_bits)
+                                    bitdepth=sdef.bits, band_lines=sdef.bands, cache_columns=sdef.columns,
+                                    spi_frame_bits=sdef.spi_bits)
                 self.screen_b = ST7789(None, None, None, self.bl_b, sdef.width, sdef.height, sdef.bits, sdef.fps, display=disp_b)
             else:
-                spi_b = SPI(id=1, baudrate=40_000_000, sck=Pin.board.SPCE_B_SCK, mosi=Pin.board.SPCE_B_MOSI)
+                spi_b = SPI(id=1, baudrate=sdef.baud, sck=Pin.board.SPCE_B_SCK, mosi=Pin.board.SPCE_B_MOSI)
                 self.screen_b = ST7789(spi_b, Pin.board.SPCE_B_CS, Pin.board.SPCE_B_DC, self.bl_b, sdef.width, sdef.height, sdef.bits, sdef.fps)
             # Need to add some handling for LED conflicts
 
