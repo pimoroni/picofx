6c8fe175c29fa334ed36f3272a04fbd89d44d0e3
ZodiusInfuser <chris@pimoroni.com>
2026-08-14 10:33:50 +0100

    picofx: give every channel a level and a colour, so a mono effect can show one

--- picofx/__init__.py
+++ picofx/__init__.py
@@ -5,7 +5,7 @@
 import time
 from machine import PWM, Pin, Timer
 
-PICOFX_VERSION = "1.1.2"
+PICOFX_VERSION = "1.1.3"
 
 
 def rgb_from_hsv(h, s, v):
@@ -182,6 +182,9 @@ class EffectPlayer:
         self.__data = [()] * self.__num_leds
         self.__updateables = set()
 
+        # A per channel scale on the output of an effect
+        self.__levels = [1.0] * self.__num_leds
+
         self.__period = 1000
         self.__timer = Timer()
         self.__paired = None
@@ -258,10 +261,7 @@ class EffectPlayer:
 
     @effects.setter
     def effects(self, effect_list):
-        effect_list = effect_list if isinstance(effect_list, list) else [effect_list] * self.__num_leds
-
-        if len(effect_list) > self.__num_leds:
-            raise ValueError(f"`effect_list` must have a length less or equal to {self.__num_leds}")
+        effect_list = self.__to_channel_list(effect_list, "effect_list")
 
         self.__updateables = set()
         for i, item in enumerate(effect_list):
@@ -306,10 +306,33 @@ class EffectPlayer:
                     self.__data[i] = tuple(rest)
 
         # Clear out excess effects
-        if len(effect_list) < self.__num_leds:
-            for i in range(len(effect_list), self.__num_leds):
-                self.__effects[i] = None
-                self.__data[i] = ()
+        for i in range(len(effect_list), self.__num_leds):
+            self.__effects[i] = None
+            self.__data[i] = ()
+
+    def __to_channel_list(self, values, name):
+        # Passes a list of values through or applies one value for every channel
+        values = values if isinstance(values, list) else [values] * self.__num_leds
+
+        if len(values) > self.__num_leds:
+            raise ValueError(f"`{name}` must have a length less or equal to {self.__num_leds}")
+
+        return values
+
+    @property
+    def levels(self):
+        return tuple(self.__levels)
+
+    @levels.setter
+    def levels(self, levels):
+        levels = self.__to_channel_list(levels, "levels")
+
+        for i, level in enumerate(levels):
+            self.__levels[i] = min(1.0, max(0.0, level))
+
+        # Reset excess levels
+        for i in range(len(levels), self.__num_leds):
+            self.__levels[i] = 1.0
 
 
 class MonoPlayer(EffectPlayer):
@@ -319,32 +342,72 @@ class MonoPlayer(EffectPlayer):
     def __show(self):
         for i in range(self.__num_leds):
             if self.__effects[i] is not None:
-                self.__leds[i].brightness(self.__effects[i](*self.__data[i]))
+                self.__leds[i].brightness(self.__effects[i](*self.__data[i]) * self.__levels[i])
 
 
-class ColourPlayer(EffectPlayer):
+class ChromaticPlayer(EffectPlayer):
+    """
+    Shared by the players whose channels can show a colour. Holds the tint a mono
+    effect is drawn in, which a mono channel has no use for.
+    """
+    def __init__(self, leds, num_leds=None):
+        super().__init__(leds, num_leds)
+        self.__colours = [(255, 255, 255)] * self.__num_leds
+
+    @property
+    def colours(self):
+        return tuple(self.__colours)
+
+    @colours.setter
+    def colours(self, colours):
+        colours = self.__to_channel_list(colours, "colours")
+
+        for i, colour in enumerate(colours):
+            if not isinstance(colour, (tuple, list)) or len(colour) != 3:
+                raise TypeError("each colour must be a tuple of three numbers")
+
+            self.__colours[i] = tuple(min(255, max(0, c)) for c in colour)
+
+        # Reset excess colours
+        for i in range(len(colours), self.__num_leds):
+            self.__colours[i] = (255, 255, 255)
+
+
+class ColourPlayer(ChromaticPlayer):
     def __init__(self, rgb_leds):
         super().__init__(rgb_leds)
 
     def __show(self):
         for i in range(self.__num_leds):
             if self.__effects[i] is not None:
-                colours = self.__effects[i](*self.__data[i])
-                if not isinstance(colours, tuple):
-                    colours = [int(colours * 255)] * 3
+                value = self.__effects[i](*self.__data[i])
+                level = self.__levels[i]
 
-                self.__leds[i].set_rgb(*colours)
+                if isinstance(value, tuple):
+                    # A colour effect brings its own colour, so only the level applies
+                    self.__leds[i].set_rgb(value[0] * level, value[1] * level, value[2] * level)
+                else:
+                    # A mono effect gives a 0.0 to 1.0 level to scale the channel's colour by
+                    value *= level
+                    r, g, b = self.__colours[i]
+                    self.__leds[i].set_rgb(r * value, g * value, b * value)
 
 
-class StripPlayer(EffectPlayer):
+class StripPlayer(ChromaticPlayer):
     def __init__(self, led_strip, num_leds=60):
         super().__init__(led_strip, num_leds)
 
     def __show(self):
         for i in range(self.__num_leds):
             if self.__effects[i] is not None:
-                colours = self.__effects[i](*self.__data[i])
-                if not isinstance(colours, tuple):
-                    colours = [int(colours * 255)] * 3
+                value = self.__effects[i](*self.__data[i])
+                level = self.__levels[i]
 
-                self.__leds.set_rgb(i, *colours)
+                if isinstance(value, tuple):
+                    r, g, b = (c * level for c in value)
+                else:
+                    value *= level
+                    r, g, b = (c * value for c in self.__colours[i])
+
+                # The strip is driven through a C binding, which wants whole numbers
+                self.__leds.set_rgb(i, int(r), int(g), int(b))
